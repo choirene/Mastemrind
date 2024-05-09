41e74389ecfa35c2e21346999c111f406484e8be
choirene <choireneh@gmail.com>
2024-05-09 19:52:28 -0400

    testing flask

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+index_bp = Blueprint("index", __name__)
+home_bp =Blueprint("home", __name__)
+
+@index_bp.route("/index", methods=["GET"])
+def index():
+    intro_text = "Hello, World!"
+    return intro_text
+@home_bp.route("/", methods=["GET"])
+def home():
+    home_text = "Welcome to Mastermind"
+    return home_text
